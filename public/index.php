cf474c83c9594db72cb77be1d905de86df61ddb7
Rafael Olegário <rafaelolegario2008@gmail.com>
2026-02-02 08:59:24 -0300

    finish terminal style

---
+++ public/index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>DailyCode</title>
+    <link rel="stylesheet" href="../css/style.css">
+    <link href="https://cdn.jsdelivr.net/npm/prismjs/themes/prism-tomorrow.css" rel="stylesheet" />
+</head>
+<body>
+    <header>
+        <nav>
+            <h1>Daily code</h1>
+            <a href=""></a>
+        </nav>
+    </header>
+    <main>
+        <h2>Can you solve the code?</h2>
+        <div class="container">
+            <div class="terminal">
+                <div class="mac">
+                    <span class="red"></span>
+                    <span class="yellow"></span>
+                    <span class="green"></span>
+                </div> 
+                <div class="code">
+                    <pre><code class="language-python">print('Hello World')</code></pre>
+                </div>
+            </div>
+        </div>  
+    </main>
+
+    <script src="https://cdn.jsdelivr.net/npm/prismjs/prism.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/prismjs/components/prism-python.min.js"></script>
+</body>
+</html>
