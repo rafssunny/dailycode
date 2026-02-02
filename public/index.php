bcc2d664c0ae72880a9c7c7df3ae0d796247adda
Rafael Olegário <rafaelolegario2008@gmail.com>
2026-02-02 15:16:46 -0300

    create buttons/options/input

--- public/index.php
+++ public/index.php
@@ -8,6 +8,9 @@
     <link href="https://cdn.jsdelivr.net/npm/prismjs/themes/prism-tomorrow.css" rel="stylesheet" />
 </head>
 <body>
+    <?php
+        $date = date('m/d/Y');
+    ?>
     <header>
         <nav>
             <h1>Daily code</h1>
@@ -20,19 +23,28 @@
         </nav>
     </header>
     <main>
-        <h2>Can you solve the code?</h2>
-        <div class="container">
-            <div class="terminal">
-                <div class="mac">
-                    <span class="red"></span>
-                    <span class="yellow"></span>
-                    <span class="green"></span>
-                </div> 
-                <div class="code">
-                    <pre><code class="language-python">print('Hello World')</code></pre>
+        <form action="" method="get">
+            <h2>What will be the output?</h2>
+            <div class="container-terminal">
+                <div class="terminal">
+                    <div class="mac">
+                        <span class="red"></span>
+                        <span class="yellow"></span>
+                        <span class="green"></span>
+                    </div> 
+                    <div class="code">
+                        <pre><code class="language-python">print('Hello World')</code></pre>
+                    </div>
                 </div>
+            </div>  
+            <div class="container-output">
+                <select name="dates" id="dates">
+                    <option value="<?=$date?>"><?=$date?></option>
+                </select>
+                <input type="text" name="output" id="output">
+                <button type="submit" class="button">Ok</button>
             </div>
-        </div>  
+        </form>
     </main>
 
     <script src="https://cdn.jsdelivr.net/npm/prismjs/prism.js"></script>
