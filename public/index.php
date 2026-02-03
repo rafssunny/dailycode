a1cee503acab641aba3bcb9b585ee66e61a7d903
Rafael Olegário <rafaelolegario2008@gmail.com>
2026-02-03 09:00:00 -0300

    fix css + create database config

--- public/index.php
+++ public/index.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>DailyCode</title>
-    <link rel="stylesheet" href="../css/style.css">
+    <link rel="stylesheet" href="css/style.css">
     <link rel="shortcut icon" href="imgs/favicon.ico" type="image/x-icon">
     <link href="https://cdn.jsdelivr.net/npm/prismjs/themes/prism-tomorrow.css" rel="stylesheet" />
 </head>
