c585bd2e5a5efd67d46b75017d1bce7d3295c0fd
Rafael Olegário <rafaelolegario2008@gmail.com>
2026-02-22 21:38:07 -0300

    change header fixed + modularize main js code

--- public/script/script.js
+++ public/script/script.js
@@ -1,5 +1,3 @@
-let date_object = new Date()
-
 const select = document.getElementById('dates')
 const form = document.getElementById('form')
 const result = document.querySelector('.result')
@@ -9,29 +7,47 @@ const streak_DOM = document.getElementById('streak')
 streak_DOM.innerHTML = streak + ' days'
 let selected_date = document.getElementById('dates').value
 
-const today_date = new Date().toISOString().split("T")[0]
 
-let before_yesterday = new Date()
-before_yesterday.setDate(before_yesterday.getDate()-2)
+// dates functions 
+function getTodayDate() {
+    return new Date().toISOString().split("T")[0]
+}
 
-const last_access = localStorage.getItem('lastAccessDate')
-const last_access_day = localStorage.getItem('lastAccessDay')
+function getTodayDayNumber() {
+    return new Date.getDate()
+}
+
+function getBeforeYesterdayDayNumber() {
+    const date = new Date()
+    date.setDate(date.getDate() - 2)
+    return d.getDate()
+}
+
+// main function for daily access
+function initializeDailyAccess() {
+    const today_date = getTodayDate()
+    const today_day = getTodayDayNumber()
+    const last_access = localStorage.getItem('lastAccessDate')
+    const last_access_day = localStorage.getItem('lastAccessDay')
+
+    if (last_access != today_date) {
+        localStorage.setItem('lastAccessDate', today_date)
+        localStorage.setItem('firstTodayHit', 'true')
+    }
+
+    if (last_access_day != today_day) {
+        localStorage.setItem('lastAccessDay', today_day)
+    }
+}
 
 // if streak dont exists, start in 0
-if (streak == null || last_access_day === before_yesterday){
+if (streak == null || last_access_day === before_yesterday) {
     streak = 0
     localStorage.setItem('streak', streak)
 }
 
-// check if is the first access of the day
-if (last_access != today_date){
-    localStorage.setItem('lastAccessDate', today_date)
-    localStorage.setItem('firstTodayHit', 'true')
-}
-
-if(last_access_day != date_object.getDate()){
-    localStorage.setItem('lastAccessDay', date_object.getDate())
-}
+// start daily access logic
+initializeDailyAccess()
 
 // update page after submit
 select.addEventListener("change", function () {
@@ -42,7 +58,7 @@ select.addEventListener("change", function () {
 if (result && result.textContent.trim() === 'Correct') {
     result.style.display = 'block'
     result.style.backgroundColor = '#268726ff';
-    if (localStorage.getItem('firstTodayHit') == 'true' && selected_date == today_date){
+    if (localStorage.getItem('firstTodayHit') == 'true' && selected_date == today_date) {
         streak = Number(++streak)
         localStorage.setItem('streak', streak)
     }
