5d95eda8a71218a3ccfef8d170f96ab33f051cf5
Rafael Olegário <rafaelolegario2008@gmail.com>
2026-03-01 12:04:52 -0300

    fix global streak bug + refactor statistics class

--- src/app/models/Statistics.php
+++ src/app/models/Statistics.php
@@ -4,15 +4,14 @@ namespace Rafa\Dailycode\models;
 use PDO;
 class Statistics
 {
-    public function updateStatistics(string $is_correct, PDO $connection)
+    public function updateStatistics(string $is_correct, PDO $connection): void
     {
         if (!$_SESSION['today_hit']) {
             if ($is_correct == 'Correct') {
-                $connection->query('UPDATE statistics SET hits = hits + 1');
-                $_SESSION['today_hit'] = true;
+                $this->setHits($connection);
+                $this->setAttempts($connection);
             } else {
-                $connection->query('UPDATE statistics SET attempts = attempts+1');
-                $_SESSION['today_attempts']++;
+                $this->setAttempts($connection);
             }
         }
     }
@@ -21,14 +20,24 @@ class Statistics
         $query = $connection->query('SELECT attempts FROM statistics;');
         return $query->fetchObject();
     }
+    private function setAttempts(PDO $connection): void
+    {
+        $connection->query('UPDATE statistics SET attempts = attempts+1');
+        $_SESSION['today_attempts']++;
+    }
     public function getHits(PDO $connection): bool|object
     {
         $query = $connection->query('SELECT hits FROM statistics');
-        return $query->fetchObject() ?? (object)['attempts'=>0];
+        return $query->fetchObject() ?? (object) ['attempts' => 0];
+    }
+    private function setHits(PDO $connection): void
+    {
+        $connection->query('UPDATE statistics SET hits = hits + 1');
+        $_SESSION['today_hit'] = true;
     }
     public function resetStatistics(PDO $connection)
     {
         session_destroy();
-        $query = $connection->query('UPDATE statistics set attempts = 0, hits = 0;') ?? (object)['attempts'=>0];
+        $query = $connection->query('UPDATE statistics set attempts = 0, hits = 0;') ?? (object) ['attempts' => 0];
     }
 }
