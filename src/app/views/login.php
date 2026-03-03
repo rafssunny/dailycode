910f51b5082a2cc579c62cdcbb720766f837f785
Rafael Olegário <rafaelolegario2008@gmail.com>
2026-03-03 08:43:43 -0300

    start admin page

---
+++ src/app/views/login.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Admin</title>
+    <link rel="stylesheet" href="../public/css/admin.css">
+</head>
+<body>
+    <div class="login-card">
+        <h2>Admin login</h2>
+        <div class="subhead">Hey, should you be here?</div>
+
+        <form action="#" method="post">
+            <div class="input-group">
+                <label for="username">user</label>
+                <input type="text" id="username" name="username" autocomplete="off">
+            </div>
+
+            <div class="input-group">
+                <label for="password">password</label>
+                <input type="password" id="password" name="password" >
+            </div>
+
+            <button type="submit" class="login-btn">Login</button>
+
+            <hr>
+        </form>
+    </div>
+</body>
+</html>
